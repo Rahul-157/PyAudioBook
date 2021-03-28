e004b58e204934bbfc04af83937c8774e3023f3f
Rahul-157 <kum28ra@gmail.com>
2021-03-28 07:16:56 +0530

    Imporved Readme, improved language choices at runtime

--- utils.py
+++ utils.py
@@ -3,15 +3,19 @@ from multiprocessing import cpu_count
 from gtts import langs
 from sys import platform as PLATFORM
 from os import system
+import logging
+logger = logging.getLogger('AudioBook')
+logger.setLevel(logging.DEBUG)
 NOTIFY_MSG = "'Remove Pages Contents, Preface, Acknowledgements, References'"
 THREADS = cpu_count()
 LANGS = langs._langs
 
 def language_check(language):
     while(language not in LANGS):
-        Warning("Language Not supported... Defaul : en English")
-        Warning("Supported Languages :"+ LANGS)
-        language = input("Enter Language Key eg: en for English")
+        logger.error("Language Not supported... Default : en English\nSupport Lanaguages are : \n")
+        for key in LANGS:
+            logger.error(key + " : "+ LANGS[key])
+        language = input("Enter Language Key eg: en for English\n")
     return language
 
 def find_last_word(start,blk_size,text):
